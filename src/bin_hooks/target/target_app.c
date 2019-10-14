cf50d41ce3918a50ae3282496f59fd31d7491bf8
SengMingYeoh <nitricrage@gmail.com>
2019-10-14 16:07:40 -0400

    Added target app compilation and hashmap to lightweight mvx

--- src/bin_hooks/target/target_app.c
+++ src/bin_hooks/target/target_app.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <lightweight_mvx.h>
 
 void call_other_function(char* string)
 {
+	mvx_start_hook(__func__);
 	printf("This is the string: %s\n", string);
+	mvx_end_hook(__func__);
 }
 
 void main ()
