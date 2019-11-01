b88aa57b3dab0496d91c6d8987415ba96cf5ecf3
xjtuwxg <xjtuwxg@gmail.com>
2019-11-01 18:00:38 -0400

    Issue #8.
    Add code to duplicate sensitive functions in two process. Works on small applications.

--- src/elfloader/src/loader/loader.h
+++ src/elfloader/src/loader/loader.h
@@ -2,8 +2,144 @@
 #define __LOADER_H
 
 #include <elf.h>
+#include <assert.h>
 #include "../../inc/log.h"
 
+typedef struct {
+	uint64_t code_start;
+	uint64_t code_end;
+	uint64_t rodata_start;
+	uint64_t rodata_end;
+	uint64_t data_start;
+	uint64_t data_end;
+} proc_info_t;
+
+//typedef struct {
+//	char *name;
+//	uint32_t offset;
+//	uint32_t flag;
+//} func_desc_t;
+
+extern int g_func_num;
+
+int init(int argc, char** argv, char** env) __attribute__ ((constructor));
+int init_conf(const char *conf_filename, func_desc_t *func);
+void gen_conf(func_desc_t *func, void *base, const char *CONF_TBL_ADDR);
+
+/**
+ * Read /proc/self/maps, find out the code/data locations
+ * */
+int read_proc(const char *bin_name, proc_info_t *pinfo)
+{
+	FILE * fproc;
+	char line[512];
+	char flag[8];
+	uint64_t start, end;
+	uint32_t file_offset, dev_major, dev_minor, inode;
+
+	log_debug("bin: %s", bin_name);
+
+	fproc = fopen("/proc/self/maps", "r");
+	while (fgets(line, 511, fproc) != NULL) {
+		sscanf(line, "%lx-%lx %31s %x %x:%x %u", &start, &end, flag, 
+				&file_offset, &dev_major, &dev_minor, &inode);
+		//printf("[%3ld] %s", strlen(line), line);
+		if (strstr(line, bin_name)) {
+		//	printf("flag %s\n", flag);
+			if (!strcmp(flag, "r-xp")) {
+				pinfo->code_start = start;
+				pinfo->code_end = end;
+			}
+			if (!strcmp(flag, "r--p")) {
+				pinfo->rodata_start = start;
+				pinfo->rodata_end = end;
+			}
+			if (!strcmp(flag, "rw-p")) {
+				pinfo->data_start = start;
+				pinfo->data_end = end;
+			}
+		}
+	}
+	fclose(fproc);
+
+	return 0;
+}
+
+/**
+ * Duplicate the proc mem (code,rodata,data)
+ * */
+void *dup_proc(proc_info_t *pinfo)
+{
+	void *mem = NULL;
+	// code, rodata, data segment size
+	uint64_t code_sz, rodata_sz, data_sz;
+	uint64_t total_sz = pinfo->data_end - pinfo->code_start;
+	// .rodata offset, .data offset
+	uint64_t rodata_off, data_off;
+
+	// calculate size and offset
+	code_sz = pinfo->code_end - pinfo->code_start;
+	rodata_sz = pinfo->rodata_end - pinfo->rodata_start;
+	data_sz = pinfo->data_end - pinfo->data_start;
+
+	rodata_off = pinfo->rodata_start - pinfo->code_start;
+	data_off = pinfo->data_start - pinfo->code_start;
+
+	assert(total_sz > 0);
+	if (code_sz+rodata_sz+data_sz != total_sz) {
+		log_warn("mem space has gap");
+	}
+	log_debug("code sz 0x%lx, rodata sz 0x%lx, total sz 0x%lx",
+			code_sz, rodata_sz, total_sz);
+
+	// allocate memory
+	mem = mmap(NULL, total_sz, PROT_WRITE|PROT_READ|PROT_EXEC,
+					MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
+	if (mem == MAP_FAILED) {
+		log_error("mmap failed %d: %s", errno, strerror(errno));
+		return mem;
+	}
+	log_debug("new mem %p, total sz 0x%lx", mem, total_sz);
+
+	// code memory content
+	memcpy(mem, (void *)(pinfo->code_start), code_sz);
+	memcpy(mem + rodata_off, (void *)(pinfo->rodata_start), rodata_sz);
+	memcpy(mem + data_off, (void *)(pinfo->data_start), data_sz);
+
+	// unmap any memory gaps in those 3 mem segments
+	if (pinfo->rodata_start - pinfo->code_end > 0) {
+		munmap(mem + code_sz, pinfo->rodata_start - pinfo->code_end);
+		log_debug("gap after code end. unmap size %lx",
+				pinfo->rodata_start - pinfo->code_end);
+	}
+	if (pinfo->data_start - pinfo->rodata_end > 0) {
+		munmap(mem + pinfo->rodata_end - pinfo->code_start,
+				pinfo->data_start - pinfo->rodata_end);
+		log_debug("gap after rodata end. unmap size %lx",
+				pinfo->data_start - pinfo->rodata_end);
+	}
+
+	return mem;
+}
+
+/**
+ * Rewrite the first several instructions.
+ * */
+int rewrite_insn(proc_info_t *pinfo, func_desc_t *func)
+{
+	int i;
+	//uint64_t code_sz = pinfo->code_end - pinfo->code_start;
+	uint64_t *p = NULL;
+
+	for (i = 0; i < g_func_num; i++) {
+		p = (uint64_t *)(pinfo->code_start + (func+i)->offset);
+		log_debug("offset 0x%x: insn code in hex %lx", (func+i)->offset, *p);
+	}
+
+	return 0;
+}
+
+#if 0
 void print_elf_header(Elf64_Ehdr *elf_header)
 {
 	log_info("%s: version %d, OS abi %d. entry 0x%lx", __func__,
@@ -162,5 +298,6 @@ int load_segments(FILE *obj, Elf64_Ehdr * elf_header, Elf64_Phdr *phdr, void **t
 
 	return 0;
 }
+#endif
 
 #endif
