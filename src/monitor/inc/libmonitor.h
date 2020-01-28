9648647240a66e0a3882cff5e612f7a157cf1424
xjtuwxg <xjtuwxg@gmail.com>
2020-01-28 11:47:11 -0500

    Separate lmvx_init/start/end code from libmonitor.c - created src/lmvx.c file. - refactor some dependency code.

--- src/monitor/inc/libmonitor.h
+++ src/monitor/inc/libmonitor.h
@@ -9,6 +9,11 @@ int ld_preload_function(int);
 void setup_ipc();
 bool is_child();
 bool is_parent();
+void store_child_pid(unsigned long pid);
+void set_mvx_active();
+void associate_all_pkeys();
+void clear_mvx_active();
+void set_mvx_active();
 extern unsigned long mvx_child_pid;
 extern unsigned long mvx_parent_pid;
 
