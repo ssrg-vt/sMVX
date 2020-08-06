7e91fb8c2c807aa50be53adddf0da9d69170f15d
abhijitm16 <abhijitm16@github.com>
2020-08-06 02:25:21 -0400

    added files to load runsc

--- gvisor_project/shared_library/prototype_runc.c
+++ gvisor_project/shared_library/prototype_runc.c
@@ -17,6 +17,10 @@
 #include <string.h>
 #include <capstone/platform.h>
 #include <capstone/capstone.h>
+#include "elf_loader.h"
+#include "wheelc/wheelc.h"
+#include <errno.h>
+#include <unistd.h>
 
 /* capstone handle */ 
 static csh handle;
@@ -533,9 +537,93 @@ int fd;
 
 }
 
-static void __myinit(void) __attribute__((constructor));
-static void __myinit(void)
+static bool read_file(const char *fname, void **buf, size_t *len)
+{
+    int fd = -1;
+    struct stat st;
+    char *tmp_buf;
+    off_t pos;
+    ssize_t read_bytes;
+
+    if (stat(fname, &st) == -1) {
+        LOG_ERR("fstat: error - %s", strerror(errno));
+        return false;
+    }
+    if (st.st_size == 0) {
+        LOG_ERR("fstat: error - st.st_size = 0");
+        return false;
+    }
+
+    tmp_buf = malloc(st.st_size);
+    if (tmp_buf == NULL) {
+        LOG_ERR("out of memory when alloc size=%lu", st.st_size);
+        return false;
+    }
+
+    fd = open(fname, O_RDONLY | O_CLOEXEC, 0);
+    if (fd < 0) {
+        LOG_ERR("open file %s error, %s", fname, strerror(errno));
+        goto out_free;
+    }
+
+    pos = 0;
+    while (pos < st.st_size) {
+        read_bytes = read(fd, tmp_buf + pos, st.st_size - pos);
+        if (read_bytes < 0) {
+            LOG_ERR("read error - %s", strerror(errno));
+            goto out_free;
+        }
+        if (read_bytes == 0)
+            break;
+        pos += read_bytes;
+    }
+
+    if (pos != st.st_size) {
+        LOG_ERR("read file incomplete, read %lu, size=%lu", pos, st.st_size);
+        goto out_free;
+    }
+
+    *buf = tmp_buf;
+    *len = st.st_size;
+    return true;
+
+out_free:
+    free(tmp_buf);
+    if (fd >= 0)
+        close(fd);
+    return false;
+}
+
+static int __myinit(void) __attribute__((constructor));
+static int __myinit(void)
 {
   proc_info_t pinfo;
+  struct elf_module *m;
+  void *bin;
+  size_t len;
+  const char *fname;
+  const char *bname;
+
+  fname = "/runsc";
+  bname = filename_from_path(fname);
+
+  if (!read_file(fname, &bin, &len)) {
+     free(bin);
+     return 0;
+  }
+
+  m = load_elf_module(bname, bin, len);
+  if (m == NULL) {
+     free(bin);
+     LOG_ERR("load_elf_module %s failed", bname);
+     return 0;
+  }
+
+  free(bin);
+
+  /* Uncomment if a function has to be run from the shared library */
+  //run_elf_module(m, "main1");
+
+  //LOG_INFO("module %s run done", m->name);
   read_proc("libc",&pinfo);
 }
